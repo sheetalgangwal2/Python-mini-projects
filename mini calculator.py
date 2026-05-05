9b9561022fed1d1b6b6f8e07794227d9484ece86
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-05-05 15:11:03 +0530

    mini calculator added

---
+++ mini calculator.py
@@ -0,0 +1,23 @@
+print("MINI CALCULATOR")
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+print()
+print("Please choose operation : \nfor SUM press 1 \nfor DIFFERNCE press 2 \nfor PRODUCT press 3 \nfor DIVISION press 4")
+print()
+x = input("choose operations : ")
+
+if(x == "1"):
+    print("SUM : ", a + b)
+if(x =="2"):
+    print("DIFFERENCE : ", a - b)
+if(x == "3"):
+    print("PRODUCT : ", a * b)
+if(x == "4"):
+    print("DIVISION : ", a / b)
+else:
+    print("please choose number between 1 to 4 for suitable results.")
+
+print()
+print("THANK YOU!")
