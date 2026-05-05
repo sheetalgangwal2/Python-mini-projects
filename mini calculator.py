47b8ebe3a68f49eb08349701a3331c3eacc01693
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-05-05 19:40:03 +0530

    mini calculator

--- mini calculator.py
+++ mini calculator.py
@@ -1,23 +1,25 @@
-print("MINI CALCULATOR")
+while True:
+    print("MINI CALCULATOR")
+    print()
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
 
-a = int(input("Enter first number : "))
-b = int(input("Enter second number : "))
+    print()
+    print("Please choose operation : \nfor SUM press 1 \nfor DIFFERNCE press 2 \nfor PRODUCT press 3 \nfor DIVISION press 4")
+    print()
+    x = input("choose operations : ")
 
-print()
-print("Please choose operation : \nfor SUM press 1 \nfor DIFFERNCE press 2 \nfor PRODUCT press 3 \nfor DIVISION press 4")
-print()
-x = input("choose operations : ")
+    if(x == "1"):
+     print("SUM : ", a + b)
+    elif(x =="2"):
+     print("DIFFERENCE : ", a - b)
+    elif(x == "3"):
+     print("PRODUCT : ", a * b)
+    elif(x == "4"):
+     print("DIVISION : ", a / b)
 
-if(x == "1"):
-    print("SUM : ", a + b)
-if(x =="2"):
-    print("DIFFERENCE : ", a - b)
-if(x == "3"):
-    print("PRODUCT : ", a * b)
-if(x == "4"):
-    print("DIVISION : ", a / b)
-else:
-    print("please choose number between 1 to 4 for suitable results.")
+    else:
+     print("please choose number between 1 to 4 for suitable results.")
 
-print()
-print("THANK YOU!")
+    print("THANK YOU!")
+    print()
