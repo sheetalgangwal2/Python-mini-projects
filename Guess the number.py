111174a81e6dc8e807b3ca1fd42a147f27a2d6d0
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-05-08 13:31:06 +0530

    Guess the Number

---
+++ Guess the number.py
@@ -0,0 +1,21 @@
+print("GUESS THE NUMBER")
+print()
+
+import random
+target = random.randint(1, 100)
+
+while True:
+    userChoice = (input("Guess the target or Quit: "))
+    if(userChoice == "Quit"):
+        break 
+
+    userChoice = int(userChoice)
+    if(userChoice == target):
+        print("WIN! you did it.")
+        break
+    elif(userChoice < target):
+        print("too low")
+    else:
+        print("too high")
+
+print("---GAME OVER---")
--- mini calculator.py
+++ mini calculator.py
@@ -23,3 +23,4 @@ while True:
 
     print("THANK YOU!")
     print()
+    
