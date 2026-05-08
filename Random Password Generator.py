6992bdd4b347af861d18558faea55fcc128bcff2
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-05-08 18:53:26 +0530

    mini calculator

---
+++ Random Password Generator.py
@@ -0,0 +1,20 @@
+print("RANDOM PASSWORD GENERATOR")
+print()
+
+import random
+import string
+
+pass_len = 10
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+#list comprehension [function for i in range(n)]
+
+# password = "".join([random.choice(charValues) for i in range(pass_len)])
+
+password = ""
+for i in range(pass_len):
+    password += random.choice(charValues)
+
+print("Your Random Password is : ", password)
+
+
