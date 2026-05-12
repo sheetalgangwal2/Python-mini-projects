173d2e6b3c953b083dc6c18978c420cfb3cc5d1e
Sheetal Gangwal <sheetalgangwal4@gmail.com>
2026-05-12 18:51:05 +0530

    password generator project

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -1,6 +1,7 @@
 print("RANDOM PASSWORD GENERATOR")
 print()
 
+
 import random
 import string
 
@@ -14,7 +15,6 @@ charValues = string.ascii_letters + string.digits + string.punctuation
 password = ""
 for i in range(pass_len):
     password += random.choice(charValues)
+    
 
 print("Your Random Password is : ", password)
-
-str = string.
